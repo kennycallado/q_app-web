59121139e6d40b067fac804d3f1c196eb6c20593
kennycallado <kennycallado@hotmail.com>
2023-07-04 13:34:56 +0200

    UPDATE Actualiza para mejor manejo de urls

---
+++ src/app/providers/constants.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://questions.kennycallado.dev/'
+const API_VERSION = 'api/v1/'
+
+export const AUTH_URL = BASE_URL + API_VERSION + 'auth/'
+export const USER_URL = BASE_URL + API_VERSION + 'user/'
+
+export const PAPER_URL = BASE_URL + API_VERSION + 'paper/'
