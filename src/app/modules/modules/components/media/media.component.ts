f6ff7262e03c6e8cd24e72e9bd9bdbdda7ce935c
kennycallado <kennycallado@hotmail.com>
2023-07-06 12:59:28 +0200

    ADD Agrega componentes media e input y actualiza slider para user media (pdt. input)

---
+++ src/app/modules/modules/components/media/media.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+
+import { DEFAULT_PICTURE_URL } from 'src/app/providers/constants';
+import { Media } from 'src/app/providers/models/media';
+
+@Component({
+  standalone: true,
+  selector: 'app-media',
+  templateUrl: './media.component.html',
+  styleUrls: ['./media.component.sass'],
+  imports: [CommonModule, YouTubePlayerModule]
+})
+export class MediaComponent implements OnInit {
+
+  @Input() media?: Media;
+
+  timeStamp: number = Date.now();
+  default_picture: string = DEFAULT_PICTURE_URL;
+
+  getLinkPicture(): string {
+    if (this.media && this.media.type === 'image') {
+      return this.media.url;
+    }
+
+    return this.default_picture + '?' + this.timeStamp;
+  }
+
+  ngOnInit(): void {
+    // Este código carga el reproductor de la API en un iframe de manera asíncrona, siguiendo las instrucciones:
+    // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+    if (!this.media) this.media = { id: 0, url: this.getLinkPicture(), type: 'image' }
+
+    if (this.media.type === 'video') {
+      const tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      document.body.appendChild(tag);
+    }
+
+    return ;
+  }
+}
--- src/app/modules/modules/components/slider/slider.component.ts
+++ src/app/modules/modules/components/slider/slider.component.ts
@@ -1,7 +1,14 @@
 import { register } from 'swiper/element/bundle'
 
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Component, OnInit, Signal, ViewEncapsulation, inject, signal } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+
+import { DEFAULT_PICTURE_URL } from 'src/app/providers/constants';
+import { PaperService } from 'src/app/providers/services/papers_api';
+import { PubPaper, PubPaperPush } from 'src/app/providers/models/paper';
+import { MediaComponent } from '../media/media.component';
+import { InputComponent } from '../input/input.component';
 
 @Component({
   standalone: true,
@@ -11,11 +18,59 @@ import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Component, OnInit, ViewEncapsula
   styleUrls: ['./slider.component.sass'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   // imports: [CommonModule, InputComponent, MediaComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, MediaComponent]
 })
 export class SliderComponent implements OnInit, AfterViewInit{
+  #router   = inject(Router)
+  #route    = inject(ActivatedRoute)
+  #paperSvc = inject(PaperService)
 
-  ngOnInit(): void { }
+  paper: Signal<PubPaper>
+  answer: string = ''
+
+  // timeStamp: number = Date.now()
+  // default_picture = DEFAULT_PICTURE_URL
+
+  allowSlideNext: boolean = true;
+  reachedEnd: boolean = false;
+
+  reachEnd() {
+    this.reachedEnd = true;
+    this.checkCompleted();
+  }
+
+  checkCompleted() {
+    return true
+  }
+
+  submitAndExit() {
+    /* some staffs */
+    this.#router.navigate(['/module']);
+  }
+
+  next(event: string) {
+    // clean the answer
+    this.answer = ''
+
+  }
+
+  prev(event: string) {
+    // clean the answer
+    this.answer = ''
+
+  }
+
+  showResource() {
+    console.log(this.paper())
+  }
+
+  ngOnInit(): void {
+    this.#route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id')!);
+
+      this.paper = signal(this.#paperSvc.papers().find(paper => paper.id == id))
+      })
+  }
 
   ngAfterViewInit(): void {
     register()
