7b0a2d7aedcec4135bec04a3231f4cd7565508e2
kennycallado <kennycallado@hotmail.com>
2023-07-12 16:14:29 +0200

    add Agrega swUpdate onInit de la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { SwUpdate, VersionEvent } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  #swUpdate = inject(SwUpdate)
   title = 'q_app-web';
+
+  ngOnInit() {
+    if (this.#swUpdate.isEnabled) {
+      this.#swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
+        if (event.type === 'VERSION_READY') {
+          if(confirm("New version available. Load New Version?")) window.location.reload()
+        }
+      })
+
+      // this.#swUpdate.checkForUpdate().then(() => {
+      //   console.log('checked for update');
+      // }).catch(err => {
+      //   console.error('error when checking for update', err);
+      // })
+
+      // this.#swUpdate.available.subscribe(() => {
+      //   if(confirm("New version available. Load New Version?")) {
+      //     window.location.reload();
+      //   }
+      // });
+    }
+  }
 }
