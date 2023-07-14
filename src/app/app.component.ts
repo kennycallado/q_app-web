9ca368ac4c18feb7deef64126de03d171f1d9e24
kennycallado <kennycallado@hotmail.com>
2023-07-14 10:10:04 +0200

    UPDATE Reorder webpush listener and subscriber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { SwUpdate, VersionEvent } from '@angular/service-worker';
+import { SwPush, SwUpdate, VersionEvent } from '@angular/service-worker';
+import { PushService } from './providers/services/webpush';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +8,22 @@ import { SwUpdate, VersionEvent } from '@angular/service-worker';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-  #swUpdate = inject(SwUpdate)
-  title = 'q_app-web';
+  #swUpdate   = inject(SwUpdate)
+  #swPush     = inject(SwPush)
+  #webpushSvc = inject(PushService)
+
+  title = 'q-app-web';
 
   ngOnInit() {
+    if (this.#swPush.isEnabled) this.#webpushSvc.listen()
+
     if (this.#swUpdate.isEnabled) {
       this.#swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
         if (event.type === 'VERSION_READY') {
           if(confirm("New version available. Load New Version?")) window.location.reload()
         }
       })
-
-      // this.#swUpdate.checkForUpdate().then(() => {
-      //   console.log('checked for update');
-      // }).catch(err => {
-      //   console.error('error when checking for update', err);
-      // })
-
-      // this.#swUpdate.available.subscribe(() => {
-      //   if(confirm("New version available. Load New Version?")) {
-      //     window.location.reload();
-      //   }
-      // });
     }
+
   }
 }
--- src/app/modules/main/components/home/home.component.ts
+++ src/app/modules/main/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core'
 
-import { Message } from 'src/app/providers/models/message';
-import { MessageService } from 'src/app/providers/services/message';
+import { Message } from 'src/app/providers/models/message'
+import { MessageService } from 'src/app/providers/services/message'
+import { PushService } from 'src/app/providers/services/webpush'
 
 @Component({
   selector: 'app-home',
@@ -10,22 +11,30 @@ import { MessageService } from 'src/app/providers/services/message';
 })
 export class HomeComponent {
   #messageSvc = inject(MessageService)
+  #webpushSvc = inject(PushService)
 
   messages = this.#messageSvc.messages
 
-  subscribeNotifications() {}
+  constructor() {
+    this.#webpushSvc.subscribe()
+  }
+
+  // unused
+  subscribeNotifications() {
+    this.#webpushSvc.subscribe()
+  }
 
   addMessage() {
     let message = new Message().new({
       title: 'New message',
       body: 'This is a new message',
       data: { type: 'info', content: ['This is an important message to read', '🔖 🔖 🔖'] }
-    });
+    })
 
-    this.#messageSvc.add(message);
+    this.#messageSvc.add(message)
   }
 
   removeMessage(id: number) {
-    this.#messageSvc.remove(id);
+    this.#messageSvc.remove(id)
   }
 }
