2256b54a6b818db88d6fb76b3c05d5fefbbb3136
kennycallado <kennycallado@hotmail.com>
2023-07-16 19:09:35 +0200

    UPDATE Actualiza webpush para ajustar con nuevo message service (work in progress)

--- src/app/providers/services/webpush.ts
+++ src/app/providers/services/webpush.ts
@@ -5,6 +5,7 @@ import { SwPush } from "@angular/service-worker"
 import { VAPID_PUBLIC_KEY } from "../constants"
 import { MessageService } from "./message"
 import { Message } from "../models/message"
+import { AuthService } from "./auth"
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,7 @@ export class PushService {
   #http           = inject(HttpClient)
   #messageSvc     = inject(MessageService)
   #swPush         = inject(SwPush)
+  #authSvc        = inject(AuthService)
 
   subscribe() {
     this.#swPush.requestSubscription({ serverPublicKey: VAPID_PUBLIC_KEY })
@@ -21,12 +23,28 @@ export class PushService {
   }
 
   private sendSubscrition(sub: PushSubscription) {
-    // this.#http.post("http://localhost:2000/api/v1/push/subs", sub).subscribe()
-    this.#http.post<any>('https://questions.kennycallado.dev/api/v1/push/subs', sub).subscribe()
+    let headers = {
+      Authorization: 'Bearer ' + this.#authSvc.access_token,
+      ContentType: 'application/json'}
+
+    let to_server = {
+      user_id: 1,
+      web_token: sub.toJSON()
+    }
+
+    // this.#http.put("http://localhost:9000/api/v1/messaging/token/1", to_server, { headers }).subscribe()
+    this.#http.put("http://questions.kennycallado.dev/api/v1/messaging/token/1", to_server, { headers }).subscribe()
   }
 
   listen() {
-    this.#swPush.messages.subscribe((message: any) => {
+    this.#swPush.messages.subscribe((message_come: any) => {
+
+      // console.log("Message received from server: ", message)
+      // console.log("Parse", JSON.parse(message))
+      // console.log(message.notification)
+
+      let message = JSON.parse(message_come)
+      console.log({ message })
 
       let new_message = new Message().new({
         title: message.notification.title,
