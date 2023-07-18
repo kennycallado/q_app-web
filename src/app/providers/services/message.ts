b1c7fdceea2b29cf88bd97e5619614bff563fe14
kennycallado <kennycallado@hotmail.com>
2023-07-18 15:32:49 +0200

    IMPL MessageService implementa destructor

--- src/app/providers/services/message.ts
+++ src/app/providers/services/message.ts
@@ -2,18 +2,22 @@ import { Injectable, computed, effect, inject, signal } from "@angular/core";
 
 import { Message } from "../models/message";
 import { StorageService } from "./storage";
+import { DestructorService } from "./destructor";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
   #storageSvc = inject(StorageService)
+  #destrSvc   = inject(DestructorService)
 
   #messages   = signal<Message[]>(this.#storageSvc.get('messages') as Message[] || [] as Message[]);
   messages    = computed(() => this.#messages())
   update      = effect(() => this.#storageSvc.set('messages', this.#messages()))
 
   constructor() {
+   this.#destrSvc.add(() => this.destructor())
+
     if (this.#messages().length === 0) {
       let firstMessage = new Message().new({
         title: 'Welcome to Q App',
@@ -34,5 +38,9 @@ export class MessageService {
 
   remove(id: number) {
     this.#messages.set(this.#messages().filter(message => message.id !== id))
+  }
+
+  destructor() {
+    this.#messages.set([])
   }
 }
