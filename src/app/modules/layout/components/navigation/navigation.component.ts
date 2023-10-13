6967fed3983e4daeedf464105b46de89462ef9f6
kennycallado <kennycallado@hotmail.com>
2023-10-13 12:07:05 +0200

    FEAT Auto clouse menu detection

--- src/app/modules/layout/components/navigation/navigation.component.ts
+++ src/app/modules/layout/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, effect, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/providers/services/auth';
 
@@ -9,10 +9,21 @@ import { AuthService } from 'src/app/providers/services/auth';
 })
 export class NavigationComponent {
   #authSvc = inject(AuthService)
+  #eRef     = inject(ElementRef)
   #router  = inject(Router)
 
   is_authenticated: boolean;
   update = effect(() => this.is_authenticated = this.#authSvc.access_token !== '' )
+
+  // detect click outside to close the menu
+  @ViewChild('burger') burger: ElementRef
+  @HostListener('document:click', ['$event.target'])
+  clickout(element: HTMLElement) {
+    if (!this.#eRef.nativeElement.contains(element)) {
+      if (this.burger.nativeElement.classList.contains('collapsed')) return ;
+      this.burger.nativeElement.click()
+    }
+  }
 
   goToHome() {
     this.#router.navigate(['/home']);
